40e1f4cfab1b9231fba4d9abc18edff7a877c040
Time-Lord-Victorious <junshenlee2017@gmail.com>
2024-10-24 17:30:56 +0800

    cart finna done

--- src/cart/CartContext.jsx
+++ src/cart/CartContext.jsx
@@ -5,28 +5,56 @@ const CartContext = createContext();
 const cartReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_TO_CART': {
-            const existingItemIndex = state.findIndex(item => item.id === action.payload.id);
-            if (existingItemIndex >= 0) {
-                const updatedCart = [...state];
-                updatedCart[existingItemIndex].quantity += 1;
-                return updatedCart;
-            } else {
-                return [...state, { ...action.payload, quantity: 1 }];
+            console.log(state);
+            //array.some searches an element in the array, if it's found-- it will return true
+
+            console.log(state)
+
+            const originalPrice = state.totalPrice
+            const newTotalPrice = originalPrice + action.payload.price
+
+            const updatedProducts = [...state.products, action.payload];
+
+
+
+            return {
+                products: updatedProducts,
+                totalPrice: newTotalPrice,
+            }
+
+        }
+
+
+        case 'REMOVE_FROM_CART': {
+            const originalPrice = state.totalPrice
+            const productToRemove = state.products[action.payload];
+            const newTotalPrice = originalPrice - productToRemove.price
+
+
+            let existingProducts = [...state.products];
+            existingProducts.splice(action.payload, 1)
+
+
+            console.log(existingProducts)
+
+
+            return {
+                totalPrice: newTotalPrice,
+                products: existingProducts,
             }
         }
 
-        case 'REMOVE_FROM_CART': {
-            return state.filter(item => item.id !== action.payload.id);
-        }
-
         default: {
-            return state;
+            return state.products;
         }
     }
 };
 
 export const CartProvider = ({ children }) => {
-    const [cart, dispatch] = useReducer(cartReducer, []);
+    const [cart, dispatch] = useReducer(cartReducer, {
+        totalPrice: 0,
+        products: [],
+    });
 
     return (
         <CartContext.Provider value={{ cart, dispatch }}>
--- src/cart/cart.jsx
+++ src/cart/cart.jsx
@@ -1,33 +1,35 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { useCart } from './CartContext';
 
 const CartSystem = () => {
-    const [totalPrice, setTotalPrice] = useState(0); //initial price is 0
+
 
     const { cart, dispatch } = useCart();
 
-    const removeFromCart = (product) => { dispatch({ type: 'REMOVE_FROM_CART', payload: product }) }
+    const removeFromCart = (indexToRemove) => { dispatch({ type: 'REMOVE_FROM_CART', payload: indexToRemove }) }
+
+    console.log(cart)
 
     return (
         <div>
             <h2>Your Cart</h2>
-            {cart.length === 0 ? (
-                <p>Your cart is empty...</p>
+            {cart.products.length === 0 ? (
+                <p>Your cart is empty bro 💀💀💀...</p>
             ) : (
                 <ul>
-                    {cart.map((item) => {
+                    {cart.products.map((item, index) => {
 
                         return (
                             <li key={item.id}>
                                 {item.name} - RM {item.price}{' '}
-                                <button onClick={() => removeFromCart(item)}>Delete Item</button>
+                                <button onClick={() => removeFromCart(index)}>Delete Item</button>
                             </li>
                         )
                     })}
 
                     <div>
                         <p>Total price: </p>
-                        <p></p>
+                        <p>{cart.totalPrice}</p>
                     </div>
                 </ul>
             )}
